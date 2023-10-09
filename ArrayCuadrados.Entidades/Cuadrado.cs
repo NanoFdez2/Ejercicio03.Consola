0ad71545578ff832865e4c79fdc8c047e6c7fd97
Nano <martinianofernandez.mf@gmail.com>
2023-10-09 10:26:10 -0300

    Agregar archivos de proyecto.

---
+++ ArrayCuadrados.Entidades/Cuadrado.cs
@@ -0,0 +1,11 @@
+﻿namespace ArrayCuadrados.Entidades
+{
+    public class Cuadrado
+    {
+        private const int _cantidadLados = 4;
+        private int _medidaLado;
+        public Cuadrado(int medidaLado) {
+        _medidaLado = medidaLado;
+        }
+    }
+}
---
+++ Ejercicio03.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicio03.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado[] arrayCuadrados = new Cuadrado[5];
+            for (int i = 0; i < arrayCuadrados.Length; i++)
+            {
+                Console.Write("Ingrese el valor del lado:");
+                arrayCuadrados[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Array completo.");
+            Console.Clear();
+            foreach ( var item in arrayCuadrados )
+            {
+                Console.WriteLine($"{item}");
+            }
+
+        }
+    }
+}
